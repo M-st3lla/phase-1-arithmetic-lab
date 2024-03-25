926a2f0d13456a1aee3250b837ae0211489e9228
its-mst <stellaoluoch231@gmail.com>
2024-03-25 11:05:36 -0400

    Initial commit

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+ let num1=Math.floor(Math.random()*10)+1;
+ let num2=Math.floor(Math.random()*10)+1;
+ const multiply=num1*num2;
+
+ let random=Math.floor(Math.random()*10) + 1;
+ console.log(random);
+
+
+let num3=Math.floor(Math.random()*10)+1;
+let num4=Math.floor(Math.random()*10)+1;
+const mod=(num3%num4)==4
+
+
+
+
+
+
+
